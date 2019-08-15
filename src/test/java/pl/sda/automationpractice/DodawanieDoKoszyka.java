1cffa085702d5cdd1613521eab4b101cde195716
gceler <grgc88@gmail.com>
2019-08-15 16:21:24 +0200

    - added @Before and @After with browser initiation, configuration and shutdown; - removed explicit waits in favour of implicit wait; - test now adds 2 pieces of the 4th item to cart before proceeding to checkout; - added assertion for the number of pieced of the particular item at checkout;

--- src/test/java/pl/sda/automationpractice/DodawanieDoKoszyka.java
+++ src/test/java/pl/sda/automationpractice/DodawanieDoKoszyka.java
@@ -1,5 +1,8 @@
 package pl.sda.automationpractice;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
@@ -7,21 +10,52 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class DodawanieDoKoszyka {
+    ChromeDriver przegladarka;
+    Actions actions;
+    List<WebElement> przyciski;
+    WebElement kontynuuj;
+    WebElement kasa;
+    String iloscProduktow;
+
+    @Before
+    public void otwarcie() {
+        przegladarka = new ChromeDriver();
+        actions = new Actions(przegladarka);
+        przegladarka.manage().window().setSize(new Dimension(1024,768));
+        przegladarka.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void zamkniecie() {
+        przegladarka.quit();
+    }
     @Test
     public void dodajProduktDoKoszyka() {
-        ChromeDriver przegladarka = new ChromeDriver();
-        Actions akcje = new Actions(przegladarka);
-
-        przegladarka.manage().window().setSize(new Dimension(1024,768));
 
         przegladarka.get("http://automationpractice.com/index.php");
-        List<WebElement> przyciski = przegladarka.findElementsByCssSelector("#homefeatured .ajax_add_to_cart_button");
 
-        akcje.moveToElement(przyciski.get(3)).perform();
+        przyciski = przegladarka.findElementsByCssSelector("#homefeatured .ajax_add_to_cart_button");
+
+        //actions.moveToElement(przyciski.get(3)).perform(); - u mnie przechodzi bez tego
 
         przyciski.get(3).click();
 
+        kontynuuj = przegladarka.findElementByCssSelector(".continue");
+
+        kontynuuj.click();
+
+        przyciski.get(3).click();
+
+        kasa = przegladarka.findElementByCssSelector(".button-medium");
+
+        kasa.click();
+
+        iloscProduktow = przegladarka.findElementByCssSelector(".cart_quantity_input").getAttribute("value");
+
+        Assert.assertEquals ("2",iloscProduktow);
     }
 }
+
